b92a755257f73898ccaac6131c52be5864b0bbe0
bruno santos paz <brunoeduardo9@hotmail.com>
2023-01-11 21:08:38 +0000

    atualiazçoes com templates

---
+++ recipes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+   return render(request,'recipes/home.html',context={'nome': 'bruno santos'})
+
+
+def sobre(request):
+   return HttpResponse('sobre2')
