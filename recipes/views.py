092b329e709fb1e2668509b0b61e45b40105bfcc
bruno santos paz <brunoeduardo9@hotmail.com>
2023-01-14 13:50:15 +0000

    atualizaçao de site remoçao de contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
    return render(request,'recipes/home.html',context={'nome': 'bruno santos'})
 
-def contato(request):
-   return HttpResponse('contato1')
-
-
-def sobre(request):
-   return HttpResponse('sobre2')
