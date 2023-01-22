be694482d833176ba335eb0c63779ff27253aa61
bruno santos paz <brunoeduardo9@hotmail.com>
2023-01-22 14:14:04 +0000

    atualizando urls e atribuindo url de entrada

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
    return render(request,'recipes/pages/home.html',context={'nome': 'bruno santos'})
 
+def recipe(request, id):
+   return render(request,'recipes/pages/recipe-view.html',context={'nome': 'bruno santos'})
+
